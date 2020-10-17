ff448a8c0053463f09b3b2b879444f5c4e41d73a
Kashyap Aghera <kaagh.10@gmail.com>
2020-10-17 02:08:40 -0400

    uploading portfolio files

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,57 +9,63 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hi! My Name is ',
+  name: 'Kashyap Aghera',
+  subtitle: "I'm a Full Stack Developer",
+  cta: 'Know More',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'kash.png',
+  paragraphOne:
+    "I'm a Full Stack Web Developer with a science background that has skills in both the front-end of web applications as well as the back-end.",
+  paragraphTwo: 'I am very passionate about building software and creating effective solutions.',
+  paragraphThree:
+    'Always eager to learn new skills and knowledge related programming that I can share with the world to utilize.',
+  resume:
+    'https://docs.google.com/document/d/1z-BxhHD5Cu-aNbMkFEP4_syzfFkmf2lxBaXbZZgjg-0/edit?usp=sharing',
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'recipeapp.png',
+    title: 'Recipe Generator',
+    info:
+      '“What’s In My Fridge” is an easy to use application that grants any novice chef or culinary hobbyist a wide range of delectable recipes made from any of the ingredients found in their fridge.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://kaghera.github.io/Recipe_API/',
+    repo: 'https://github.com/Kaghera/Recipe_API',
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'goads.png',
+    title: 'goAds.com',
+    info:
+      'An online classified advertising services platform which allows users to exchange goods and services. Perfect marketplace to buy and sell products locally.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://fathomless-hollows-88186.herokuapp.com/',
+    repo: 'https://github.com/Kaghera/goAds.com',
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'dayplanner.png',
+    title: 'Day Planner',
+    info:
+      'A simple calendar application that allows the user to save events for each hour of the day.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://kaghera.github.io/Day-Planner/',
+    repo: 'https://github.com/Kaghera/Day-Planner',
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
-  btn: '',
-  email: '',
+  btn: "Let's Connect!",
+  email: 'kashyapaghera@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,22 +73,22 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'codepen',
-      url: '',
+      url: 'https://codepen.io/Kashmoney',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/kashyap-aghera-8070a01a4/',
     },
     {
       id: nanoid(),
       name: 'github',
+      url: 'https://github.com/Kaghera',
+    },
+    {
+      id: nanoid(),
+      name: 'twitter',
       url: '',
     },
   ],
@@ -90,5 +96,5 @@ export const footerData = {
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
