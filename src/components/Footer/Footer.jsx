aa1b8c42e883a0e46c1e3ec30318af070343796f
Kashyap Aghera <kaagh.10@gmail.com>
2020-11-10 16:27:31 -0500

    modified files

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -38,9 +38,9 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by{' '}
+          © {new Date().getFullYear()}{' '}
           <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
-            Jacobo Martínez
+            All Rights Reserved
           </a>
         </p>
 
